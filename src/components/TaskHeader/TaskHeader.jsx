3a995cce23c0a70fdf960b63a67c31e2a00eb71c
Ambuj Kumar <89453669+AmbujKrAK@users.noreply.github.com>
2022-06-08 20:33:19 +0530

    most of the task added

--- src/components/TaskHeader/TaskHeader.jsx
+++ src/components/TaskHeader/TaskHeader.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import styles from "./taskHeader.module.css";
 
-const TaskHeader = () => {
+const TaskHeader = ({ tasks }) => {
   // sample values to be replaced
-  let totalTask = 0;
-  let unCompletedTask = 0;
-
+  let totalTask = tasks.length;
+  let unCompletedTask = tasks.filter((task) => !task.done).length;
   // NOTE: do not delete `data-testid` key value pair
   return (
     <div data-testid="task-header" className={styles.taskHeader}>
